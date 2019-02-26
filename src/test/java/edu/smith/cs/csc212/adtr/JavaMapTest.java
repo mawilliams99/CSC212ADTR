f87a2160e42a51424df0bc89d682a633c21c4d81
madisonwilliams <madisonwilliams@131.229.197.40>
2019-02-25 21:35:29 -0500

    update

--- src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
@@ -12,4 +12,43 @@ public class JavaMapTest {
 		MapADT<String, Integer> empty = new JavaMap<>();
 		assertEquals(empty.size(), 0);
 	}
+	
+	public void testPut() {
+		MapADT<String, Integer> object = new JavaMap<>();
+		object.put("hey", 1);
+		
+		assertEquals(object.size(), 1);
+	}
+	public void testGet() {
+		MapADT<String, Integer> object = new JavaMap<>();
+		
+		assertEquals(object.get(null), null);
+	}
+	public void testSize() {
+		MapADT<String, Integer> object = new JavaMap<>();
+		object.put("hey", 1);
+		
+		assertEquals(object.size(), 1);
+	}
+	public void testRemove() {
+		MapADT<String, Integer> object = new JavaMap<>();
+		object.put("hey", 1);
+		object.remove("hey");
+		
+		assertEquals(object.size(), 0);
+	}
+	public void getKeys() {
+		MapADT<String, Integer> object = new JavaMap<>();
+		object.put("hey", 1);
+		object.getKeys(); 
+		
+		assertEquals(object.getKeys(), "hey");
+	}
+	public void getEntries() {
+		MapADT<String, Integer> object = new JavaMap<>();
+		object.put("hey", 1);
+		object.getEntries(); 
+		
+		assertEquals(object.getEntries(), 1);
+	}
 }
--- src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
@@ -11,6 +11,49 @@ public class JavaSetTest {
 	@Test
 	public void testEmpty() {
 		SetADT<String> empty = new JavaSet<>();
+		
 		assertEquals(empty.size(), 0);
 	}
+	public void testInsert() {
+		SetADT<String>  object = new JavaSet<>();
+		
+		// insert something into object
+		object.insert("1");
+		
+		assertEquals(object.size(), 1);
+	}
+	public void testContains () {
+		SetADT<String>  object = new JavaSet<>();
+		SetADT<String>  object2 = new JavaSet<>();
+		
+		object.insert("1");
+		object2.insert("1");
+		
+		assertEquals(object.contains("1"), object2.contains("1"));
+	}
+	public void testRemove () {
+		SetADT<String>  object = new JavaSet<>();
+		
+		object.insert("1");
+		object.remove("1");
+		
+		assertEquals(object.size(), 0);
+	}
+	public void toList () {
+		SetADT<String>  object = new JavaSet<>();
+		
+		object.toList(); 
+		
+		assertEquals(object, List);
+	}
+	public void toJava () {
+		SetADT<String>  object = new JavaSet<>();
+		
+		object.toJava(); 
+		
+		assertEquals(object, );
+	}
+	
+	
+	
 }
